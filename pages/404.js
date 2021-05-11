e711526d59a723bc9a5864c89897251766d55f8e
Alexander Burdiss <aburdiss@icloud.com>
2021-05-10 23:01:16 -0400

    Added 404, favicon, and image for background

---
+++ pages/404.js
@@ -0,0 +1,9 @@
+import Layout from "../components/Layout/Layout";
+
+export default function FourOhFour() {
+  return (
+    <Layout>
+      <div>Hello, 404 Error!</div>
+    </Layout>
+  );
+}
--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,6 @@ import Head from "next/head";
 import Image from "next/image";
 
 import styles from "./Home.module.css";
-import Header from "../components/Header/Header";
 import Layout from "../components/Layout/Layout";
 
 export default function Home() {
@@ -13,14 +12,16 @@ export default function Home() {
         <meta name="description" content="A website by Daniel Stigmon" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
       <main className={styles.main}>
+        {/* <div>
+          <Image src="/home_background.jpg" layout="fill" />
+        </div> */}
         <h1>The Bedtime Project</h1>
         <div>Your pathway from Virtual to Reality</div>
         <div>
           Welcome to Bridge, a place where your imagination is the limit and you
           are the main character of your story. Step into one of our unique
-          worlds and become a part of your next fairy tale.{" "}
+          worlds and become a part of your next fairy tale.‰
         </div>
       </main>
     </Layout>
